b558e145ab5f6c3026baebbf7808390d4c7077d3
Keith Harrison <ex.curiositatem.scientia@gmail.com>
2018-02-12 20:26:52 -0700

    Programming Exercise 1 with Screenshots.

---
+++ Introduction.java
@@ -0,0 +1,15 @@
+/** Program: Introduction Program
+ * File: Introduction.java
+ * Summary: This program is an introduction.
+ *
+ * Author: Keith Harrison
+ * Date: 2/12/2018
+ * 
+ */
+public class Introduction {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Week1Assignment.java
@@ -0,0 +1,24 @@
+/** Program: Keith Preferences Program
+ * File: Week1Assignment.java
+ * Summary: This program lists my name, favorite color and what I would do if
+ *          I won the lottery.
+ *
+ * Author: Keith Harrison
+ * Date: 2/12/2018
+ * 
+ */
+
+public class Week1Assignment {
+
+    public static void main(String[] args) {
+        // Print to console my name.
+        System.out.println("My name is Keith Harrison.");
+        // Print to console my favorite color.
+        System.out.println("My favorite color is black.");
+        // Print to console what I would do if I won the lottery.
+        System.out.println("If I won the lottery, I would build a castle and " + 
+                "build a round table for my family, friends, instructors and " +
+                "classmates.");
+    }
+    
+}
